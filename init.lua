129edb023b48ef0a21f95d9c2604b32498a7fee7
Hunter Egnatis <hunteregnatis@gmail.com>
2024-08-12 12:01:47 -0400

    Added nvim-navigation plugin

--- init.lua
+++ init.lua
@@ -800,10 +800,11 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.vim-tmux-navigator',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend -
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/kickstart/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,3 @@
+return {
+  'christoomy/vim-tmux-navigator',
+}
