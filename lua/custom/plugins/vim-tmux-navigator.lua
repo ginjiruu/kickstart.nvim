67d1d86882e32831cacd6ff8d3eb0c0077ae9c12
Hunter Egnatis <hunteregnatis@gmail.com>
2024-09-05 15:08:29 -0400

    fixed issue with binds for arrowkeys

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -1,5 +1,6 @@
 return {
   'christoomey/vim-tmux-navigator',
+  event = 'VimEnter', -- Ensure it loads when Neovim starts
   cmd = {
     'TmuxNavigateLeft',
     'TmuxNavigateDown',
@@ -8,10 +9,9 @@ return {
     'TmuxNavigatePrevious',
   },
   keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-s>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-t>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-n>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    { '<c-q>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    { '<c-left>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<c-down>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-up>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-right>', '<cmd>TmuxNavigateRight<cr>' },
   },
 }
