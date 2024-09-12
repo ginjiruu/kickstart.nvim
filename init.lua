37a929227839b44fbe2dfcbfffff8d99da36ffb2
Hunter Egnatis <hunteregnatis@gmail.com>
2024-09-12 10:02:45 -0400

    changed color scheme to adwaita

--- init.lua
+++ init.lua
@@ -708,16 +708,22 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'Mofiqul/adwaita.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end, -- configure and set on startup
+    config = function()
+      -- vim.g.adwaita_darker = true -- for darker version
+      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      vim.g.adwaita_transparent = true -- makes the background transparent
+      vim.cmd 'colorscheme adwaita'
     end,
   },
 
